7f5828e9edf10cdbc425bdf3d5b908894b3e0fcc
MalindiVal <63977034+MalindiVal@users.noreply.github.com>
2026-02-16 17:28:32 +0100

    Creation de nouveaux dao pour les differentes tables de la base de données

---
+++ API/Data/Interfaces/IEtatDAO.cs
@@ -0,0 +1,11 @@
+﻿using LogicLayer;
+
+namespace API.Data.Interfaces
+{
+    public interface IEtatDAO
+    {
+        void InsertEtats(Automate automate);
+
+
+    }
+}
---
+++ API/Data/Interfaces/ITransitionDAO.cs
@@ -0,0 +1,14 @@
+﻿using LogicLayer;
+
+namespace API.Data.Interfaces
+{
+    public interface ITransitionDAO
+    {
+        /// <summary>
+        /// Insère les transitions liées à l'automate.
+        /// </summary>
+        /// <param name="automate"></param>
+        /// <exception cref="DAOError"></exception>
+        void InsertTransitions(Automate automate);
+    }
+}
