2a882704669057f84d471c52a196e3cb244d5401
MalindiVal <63977034+MalindiVal@users.noreply.github.com>
2026-02-17 05:08:06 +0100

    Comportementt retirées des classes Automate et Transitions

--- LogicLayer/Transition.cs
+++ LogicLayer/Transition.cs
@@ -12,11 +12,6 @@ namespace LogicLayer
     /// </summary>
     public class Transition
     {
-        #region Constantes     
-        private const double DecalagePremiere = 5;
-        private const double DecalageMultiple = 20;        
-        #endregion
-
         #region Attributs
         private Etat etat1;
         private Etat etat2;
@@ -49,20 +44,6 @@ namespace LogicLayer
         #endregion
 
         #region Propriétés calculées
-        /// <summary>
-        /// Booléen qui renvoie true si on doit faire une boucle
-        /// </summary>
-        public bool IsLoop => EtatDebut == EtatFinal || (EtatDebut.Position.X == EtatFinal.Position.X && EtatDebut.Position.Y == EtatFinal.Position.Y);
-
-        /// <summary>
-        /// Détermine l'angle de la transition
-        /// </summary>
-        private double Angle => Math.Atan2(EtatFinal.Position.Y - EtatDebut.Position.Y, EtatFinal.Position.X - EtatDebut.Position.X);
-
-        /// <summary>
-        /// Détermine la distance entre les deux états
-        /// </summary>
-        private double Distance => Math.Sqrt(Math.Pow(EtatFinal.Position.X - EtatDebut.Position.X, 2) + Math.Pow(EtatFinal.Position.Y - EtatDebut.Position.Y, 2));
 
         /// <summary>
         /// Coordonnée X du point de contrôle manuel (nullable).
@@ -88,121 +69,6 @@ namespace LogicLayer
             this.etat2 = fin;
         }
 
-        /// <summary>
-        /// Constructeur vide pour sérialisation ou initialisation ultérieure.
-        /// </summary>
-        public Transition(){}
-        #endregion
-
-        #region Méthodes
-        /// <summary>
-        /// Calcule la position géométrique X de l'état de départ liées a la structure logique de la transition 
-        /// </summary>
-        /// <param name="index">Nombre de transitions dans le même sens</param>
-        /// <param name="total">nombre total de transition</param>
-        /// <param name="rayon">rayon de l'état</param>
-        /// <returns>renvoit la position X du départ de la transition</returns>
-        public double GetXDepartDecale(int index, int total, double rayon)
-        {
-            double result;
-            if (IsLoop)
-            {
-                result = EtatDebut.Position.X;
-            }
-            else
-            {
-                double dx = EtatFinal.Position.X - EtatDebut.Position.X;
-                double dy = EtatFinal.Position.Y - EtatDebut.Position.Y;
-                double longueur = Math.Sqrt(dx * dx + dy * dy);
-                if (longueur == 0) longueur = 1;
-
-                double offset = (index - (total - 1) / 2.0) * DecalageMultiple + DecalagePremiere;
-                result = EtatDebut.Position.X + (rayon / 2.0) * Math.Cos(Angle) - dy / longueur * offset;
-            }
-            return result;        
-        }
-
-        /// <summary>
-        /// Calcule la position géométrique Y de l'état de départ liées a la structure logique de la transition 
-        /// </summary>
-        /// <param name="index">Nombre de transitions dans le même sens</param>
-        /// <param name="total">nombre total de transition</param>
-        /// <param name="rayon">rayon de l'état</param>
-        /// <returns>renvoit la position X du départ de la transition</returns>
-        public double GetYDepartDecale(int index, int total, double rayon)
-        {
-            double result;
-            if (IsLoop)
-            {
-                result = EtatDebut.Position.Y;
-            }
-            else
-            {
-                double dx = EtatFinal.Position.X - EtatDebut.Position.X;
-                double dy = EtatFinal.Position.Y - EtatDebut.Position.Y;
-                double longueur = Math.Sqrt(dx * dx + dy * dy);
-                if (longueur == 0) longueur = 1;
-
-                double offset = (index - (total - 1) / 2.0) * DecalageMultiple + DecalagePremiere;
-                result = EtatDebut.Position.Y + (rayon / 2.0) * Math.Sin(Angle) + dx / longueur * offset;
-            }
-            return result;
-                
-        }
-
-        /// <summary>
-        /// Calcule la position géométrique X de l'état d'arrivée liées a la structure logique de la transition 
-        /// </summary>
-        /// <param name="index">Nombre de transitions dans le même sens</param>
-        /// <param name="total">nombre total de transition</param>
-        /// <param name="rayon">rayon de l'état</param>
-        /// <returns>renvoit la position X d'arrivée de la transition</returns>
-        public double GetXArriveeDecale(int index, int total, double rayon)
-        {
-            double result;
-            if (IsLoop)
-            {
-                result = EtatFinal.Position.X;
-            }
-            else 
-            {
-                double dx = EtatFinal.Position.X - EtatDebut.Position.X;
-                double dy = EtatFinal.Position.Y - EtatDebut.Position.Y;
-                double longueur = Math.Sqrt(dx * dx + dy * dy);
-                if (longueur == 0) longueur = 1;
-
-                double offset = (index - (total - 1) / 2.0) * DecalageMultiple + DecalagePremiere;
-                result = EtatFinal.Position.X - (rayon / 2.0) * Math.Cos(Angle) - dy / longueur * offset;
-            }
-            return result;             
-        }
-
-        /// <summary>
-        /// Calcule la position géométrique Y de l'état d'arrivée liées a la structure logique de la transition 
-        /// </summary>
-        /// <param name="index">Nombre de transitions dans le même sens</param>
-        /// <param name="total">nombre total de transition</param>
-        /// <param name="rayon">rayon de l'état</param>
-        /// <returns>renvoit la position Y d'arrivée de la transition</returns>
-        public double GetYArriveeDecale(int index, int total, double rayon)
-        {
-            double result;
-            if (IsLoop)
-            {
-                result = EtatFinal.Position.Y;
-            }
-            else
-            {
-                double dx = EtatFinal.Position.X - EtatDebut.Position.X;
-                double dy = EtatFinal.Position.Y - EtatDebut.Position.Y;
-                double longueur = Math.Sqrt(dx * dx + dy * dy);
-                if (longueur == 0) longueur = 1;
-
-                double offset = (index - (total - 1) / 2.0) * DecalageMultiple + DecalagePremiere;
-                result = EtatFinal.Position.Y - (rayon / 2.0) * Math.Sin(Angle) + dx / longueur * offset;
-            }
-            return result;           
-        }
         #endregion
     }
 }
